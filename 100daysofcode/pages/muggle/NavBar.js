432c451fc73140de797b1028a4881d557750ae01
Tristan Muggridge <89533155+Tristan-Muggridge@users.noreply.github.com>
2022-10-31 22:55:32 +1000

    Starting to separate components & fixed responsive design issues

---
+++ 100daysofcode/pages/muggle/NavBar.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react"
+import Image from "next/image"
+
+import MuggleLogo from "../../public/Muggle/logo.svg"
+import {navDropDownData} from "../../data/navDropDownData"
+
+import { BiSearch, BiMenu } from 'react-icons/bi'
+
+
+export const NavBar = () => {
+
+    const navHoverables = {Study: "Study", "Current Students": "Current Students", Research: "Research", Alumni: "Alumni", About: "About", None: "None", Magnify:"Magnify"};
+    const [ hoveredNav, setHoveredNav ] = useState(navHoverables.None);
+    const [navActive, setNavActive] = useState(false);
+
+    return (
+        <>
+            {/* Navigation Bar - Desktop */}
+            <nav className="px-4 pt-4 hidden justify-between max-w-6xl align-middle items-center mx-auto -mb-[16px] md:flex">
+                <h1 className='w-36'> <a href="http://localhost:3000/Muggle"> <Image src={MuggleLogo} alt="Muggle University Logo" className='-translate-y-3'/> </a></h1>
+                <div className='flex flex-col justify-center'>
+                    <ul className=' flex justify-between gap-8 ml-auto p-4'>
+                        {
+                            ["LIBRARY", "INTERNATIONAL", "CONTACT", "UCONNECT"].map((e, indx) => {
+                            return (
+                                <li 
+                                    key={indx} 
+                                    className='text-sm text-blue-neutral hover:underline hover:cursor-pointer'
+                                > <a href="http://google.com"> {e} </a> </li>
+                            )})
+                        }
+                    </ul>
+
+                    <ul className=' flex justify-between '>
+                        {
+                            ["Study", "Current Students", "Research", "Alumni", "About"].map((e, indx) => {
+                            return (
+                            <li key={indx} 
+                                className={hoveredNav != navHoverables[e] ? 'mt-3 px-4 pb-4 text-lg text-blue-dark || select-none' : 'mt-3 px-4 pb-2 text-lg text-blue-dark select-none || border-b-blue-neutral border-b-4 cursor-pointer'}
+                                onMouseEnter={ () => { setHoveredNav(navHoverables[e])} }
+                                onMouseLeave={ () => { setHoveredNav(navHoverables.None)} }
+                            > {e} </li>
+                            )})
+                        }            
+                        <div className='p-2 text-blue-dark my-auto || hover:outline hover:outline-1 hover:outline-blue-dark hover:rounded-full hover:cursor-pointer hover:text-blue-neutral'>
+                            <BiSearch className='text-2xl blue-dark inline '/>
+                        </div>
+                    </ul>
+                </div>  
+            </nav>
+            
+            {/* DropDown Menu - Desktop */}
+            <div className={hoveredNav != navHoverables.None ? 'bg-gray-100 h-96 absolute w-full -mt-0.5' : 'hidden'} onMouseEnter={() => {setHoveredNav(hoveredNav)}} onMouseLeave={() => {setHoveredNav(navHoverables.None)}}>
+                <h2 className='p-10 text-4xl'>{hoveredNav}</h2>
+                <div className='px-6 h-20 w-full grid grid-cols-4'>    
+                    {
+                        navDropDownData[hoveredNav].Links.map((column) => {
+                            return (
+                            <ul>
+                                {column.map((row) => { return (<li className='py-1 px-4 hover:bg-gray-200 mx-2'> {row} </li>) })}
+                            </ul>
+                        )})
+                    }
+                    <div className='grid grid-cols-4'>
+                        <div className=' bg-gray-300 w-0 border h-40 -translate-y-3 col-span-1 mr-0'></div>/
+                        <div className='col-span-2 ml-0'>
+                            <p className='text-sm font-extralight'>{navDropDownData[hoveredNav].Prompt}</p>
+                            <button className='mt-4 border border-black rounded-2xl py-2 px-6 w-auto hover:bg-gray-200'>{navDropDownData[hoveredNav].ButtonText}</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Navigation Bar - Mobile */}
+            <nav className="px-4 pt-4 flex justify-between max-w-6xl align-middle items-center mx-auto -mb-[16px] md:hidden">
+                <h1 className='w-44'> <a href="http://localhost:3000/Muggle"> <Image src={MuggleLogo} alt="Muggle University Logo"/> </a></h1>
+                <BiMenu className=' w-10 h-10 || hover:text-blue-neutral hover:cursor-pointer' onClick={() => {setNavActive(!navActive)}}/>
+            </nav>
+            
+            {/* Navigation Column - Mobile */}
+            <ul className={ navActive == true ? 'bg-blue-dark absolute left-[80%] text-white font-semibold md:hidden' : 'hidden'}>
+                {
+                    [
+                        "Study",
+                        "Current Students",
+                        "Current Students",
+                        "Research",
+                        "Alumni",
+                        "About",
+                    ].map((e) => {return <li className="w-full px-4 py-6 float-left hover:bg-blue-neutral"> <a href="">{e}</a> </li>})
+                }
+            </ul>
+        </>
+    )
+}
--- 100daysofcode/pages/muggle/index.js
+++ 100daysofcode/pages/muggle/index.js
@@ -1,21 +1,20 @@
 import Head from 'next/head'
 
+import { NavBar } from './NavBar'
+
 // Icons
-import { BiSearch, BiPlay, BiPause, BiMenu, BiChevronRight } from 'react-icons/bi'
+import { BiSearch, BiPlay, BiPause, BiChevronRight } from 'react-icons/bi'
 
 // Apparently can't use img in React? Problem for another day because this works!
 import Image from "next/image"
-import MuggleLogo from "../../public/Muggle/logo.svg"
 
 import carousel1 from "../../public/Muggle/carousel1.jpg"
 import carousel2 from "../../public/Muggle/carousel2.jpg"
 import carousel3 from "../../public/Muggle/carousel3.jpg"
 
-import {navDropDownData} from "../../data/navDropDownData";
 import { useState, useEffect } from 'react'
-import { getFontDefinitionFromNetwork } from 'next/dist/server/font-utils'
 
-const navHoverables = {Study: "Study", "Current Students": "Current Students", Research: "Research", Alumni: "Alumni", About: "About", None: "None", Magnify:"Magnify"};
+// const navHoverables = {Study: "Study", "Current Students": "Current Students", Research: "Research", Alumni: "Alumni", About: "About", None: "None", Magnify:"Magnify"};
 const carouselImages = [
   {img: carousel1, alt: "", bannerHeadline: "Rise in global rankings", bannerBlurb: () => {return (<span>Muggle University has been named in the Top 301-350 in the Time Higher Education World University Rankings. <a className='text-blue-neutral hover:underline hover:cursor-pointer'> Read more. </a></span>)}}, 
   {img: carousel2, alt: "", bannerHeadline: "Applications now open", bannerBlurb: () => {return (<span> New opportunities, big ambitions. <a className='text-blue-neutral hover:underline hover:cursor-pointer'> Apply now </a> to start this November </span>)}}, 
@@ -24,14 +23,11 @@ const carouselImages = [
 
 export default function Home() {
 
-  const onResize = () => window.innerWidth > 819 ? setMobileMode(false) : setMobileMode(true)
   const updateActiveCarouselImage = (element) => {setActiveCarouselImage(element)}
 
-  const [ hoveredNav, setHoveredNav ] = useState(navHoverables.None);
+  // const [ hoveredNav, setHoveredNav ] = useState(navHoverables.None);
   const [ activeCarouselImage, setActiveCarouselImage ] = useState(0);
   const [ playing, setPlaying ] = useState(true);
-  const [ windowDimensions, setWindowDimensions ] = useState({height: 0, width: 0});
-  const [ mobileMode, setMobileMode ] = useState(false);
 
   useEffect(() => {
     if (playing == false) return;
@@ -46,93 +42,18 @@ export default function Home() {
     };
   }, [updateActiveCarouselImage, setPlaying]);
 
-
-  useEffect(() => {
-    setWindowDimensions({height: window.innerHeight, width: window.innerWidth});
-    window.innerWidth > 819 ? setMobileMode(false) : setMobileMode(true);
-    window.addEventListener('resize', onResize);
-  }, []);
-
   return (
     <div className='w-full'>
       <Head>
         <title>Mock-Muggle Branding</title>
-        <meta name="description" content="Attempting to recreate the Muggle home page to learn more about front-end development and Tailwind CSS" />
+        <meta name="description" content="Attempting to recreate the UniSQ home page to learn more about front-end development and Tailwind CSS" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Education Notice */}
-      {/* <h1 className='text-4xl absolute left-1/2 transform -translate-x-1/2 z-10'> FOR EDUCATIONAL PURPOSES </h1> */}
       <main className='bg-white'>
 
-        {/* Navigation Menu */}
-        
-          <nav className="px-4 pt-4 flex justify-between max-w-6xl align-middle items-center mx-auto -mb-[16px]">
-          { 
-            mobileMode == false 
-            ? 
-            <>
-            <h1 className='w-36'> <a href="http://localhost:3000/Muggle"> <Image src={MuggleLogo} alt="Muggle University Logo" className='-translate-y-3'/> </a></h1>
-              <div className='flex flex-col justify-center'>
-                
-                <ul className=' flex justify-between gap-8 ml-auto p-4'>
-                  {
-                    ["LIBRARY", "INTERNATIONAL", "CONTACT", "UCONNECT"].map((e, indx) => {
-                      return (
-                        <li key={indx} className='text-sm text-blue-neutral hover:underline hover:cursor-pointer'> <a href="http://google.com"> {e} </a> </li>
-                      )})
-                  }
-                </ul>
+        <NavBar />
 
-                <ul className=' flex justify-between '>
-                  {
-                    ["Study", "Current Students", "Research", "Alumni", "About"].map((e, indx) => {
-                      return (
-                        <li key={indx} 
-                            className={hoveredNav != navHoverables[e] ? 'mt-3 px-4 pb-4 text-lg text-blue-dark || select-none' : 'mt-3 px-4 pb-2 text-lg text-blue-dark select-none || border-b-blue-neutral border-b-4 cursor-pointer'}
-                            onMouseEnter={ () => {setHoveredNav(navHoverables[e])} }
-                            onMouseLeave={ () => {setHoveredNav(navHoverables.None)}}
-                        > {e} </li>
-                      )})
-                  }
-                  {/* <div className='text-lg blue-dark px-4 hover:border-b-blue-neutral hover:border-b-4 -mb-0.5 pb-6 mt-4 hover:cursor-pointer'> */}
-                  <div className='p-2 text-blue-dark my-auto || hover:outline hover:outline-1 hover:outline-blue-dark hover:rounded-full hover:cursor-pointer hover:text-blue-neutral'>
-                    <BiSearch className='text-2xl blue-dark inline '/>
-                  </div>
-              </ul>
-            </div>  
-          </> 
-          : 
-          <>
-              <h1 className='w-44'> <a href="http://localhost:3000/Muggle"> <Image src={MuggleLogo} alt="Muggle University Logo"/> </a></h1>
-              <BiMenu className=' w-10 h-10 || hover:text-blue-neutral hover:cursor-pointer'/>
-          </>
-        }
-        </nav>
-
-        
-        {/* Navigation Sub-Menus */}
-        <div className={hoveredNav != navHoverables.None ? 'bg-gray-100 h-96 absolute w-full -mt-0.5' : 'hidden'} onMouseEnter={() => {setHoveredNav(hoveredNav)}} onMouseLeave={() => {setHoveredNav(navHoverables.None)}}>
-          <h2 className='p-10 text-4xl'>{hoveredNav}</h2>
-          <div className='px-6 h-20 w-full grid grid-cols-4'>    
-            {
-              navDropDownData[hoveredNav].Links.map((column) => {
-                return (
-                  <ul>
-                    {column.map((row) => { return (<li className='py-1 px-4 hover:bg-gray-200 mx-2'> {row} </li>) })}
-                  </ul>
-              )})
-            }
-          <div className='grid grid-cols-4'>
-            <div className=' bg-gray-300 w-0 border h-40 -translate-y-3 col-span-1 mr-0'/>
-              <div className='col-span-2 ml-0'>
-                <p className='text-sm font-extralight'>{navDropDownData[hoveredNav].Prompt}</p>
-                <button className='mt-4 border border-black rounded-2xl py-2 px-6 w-auto hover:bg-gray-200'>{navDropDownData[hoveredNav].ButtonText}</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        
         {/* Covid Notice */}
         <section className=''>
           <div className='h-auto bg-blue-dark text-white '>
@@ -192,7 +113,7 @@ export default function Home() {
             </div>
             
             {/* Links to study areas */}
-            <ul className={ mobileMode == true ? 'grid grid-cols-2 gap-3' : 'grid grid-cols-3 gap-3'}>
+            <ul className='grid grid-cols-2 gap-3 md:grid-cols-3'>
               {
                 [
                   "Argiculture & Environment", "Arts & Humanities", "Aviation", 
@@ -213,7 +134,7 @@ export default function Home() {
 
             <h2 className='text-4xl mt-16 mb-8'> Other study options </h2>
 
-            <ul className={ mobileMode == true ? 'grid grid-cols-2 gap-3' : 'grid grid-cols-3 gap-3'}>
+            <ul className='grid grid-cols-2 gap-3 md:grid-cols-3'>
                 {
                   [
                     "Double degrees", "English Language", "Pathways",
@@ -232,9 +153,8 @@ export default function Home() {
             <h2 className='text-2xl mt-16 mb-5'> Already know what you want to study? </h2>
             
             <div className='mx-auto'>
-              <button className='py-4 px-8 m-5 w-22 bg-blue-neutral text-blue-dark rounded-2xl font-semibold'> Apply <BiChevronRight className='text-xl inline'/> </button>
-              {mobileMode == true ? <br /> : <></>}
-              <button className='py-4 px-8 m-5 text-blue-neutral bg-blue-dark rounded-2xl font-semibold'> Enquire <BiChevronRight className='text-xl inline'/> </button>
+              <button className='py-4 px-8 m-5 w-22 bg-blue-neutral text-blue-dark rounded-2xl font-semibold md:w-40 w-full'> Apply <BiChevronRight className='text-xl inline'/> </button>
+              <button className='py-4 px-8 m-5 text-blue-neutral bg-blue-dark rounded-2xl font-semibold md:w-40 w-full'> Enquire <BiChevronRight className='text-xl inline'/> </button>
             </div>
           </section>
 
